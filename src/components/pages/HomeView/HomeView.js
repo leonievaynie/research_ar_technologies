d243f8e5bd24498a8d17c6975efb8234fbef8de9
Leonie <leonie.jacobs@vaynie.com>
2022-03-15 09:21:39 +0100

    route system

---
+++ src/components/pages/HomeView/HomeView.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class HomeView extends Component {
+    render() {
+        return (
+            <>
+                <h2>Welcome to the research!</h2>
+                <a href="/three-js">Three.js</a>
+            </>
+        );
+    }
+}
+
+export default HomeView;
